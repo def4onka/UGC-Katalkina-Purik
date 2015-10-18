15970abbf8482b85e2395a5d1dc60762dcc328c1
Porik Yana <purik.yana@yandex.ru>
2015-10-18 15:10:07 +0300

    sql fali war corrected

--- tables.sql
+++ tables.sql
@@ -3,28 +3,32 @@ create table Employee (
 	firstname text not null, 
 	secondname text, 
 	passport bigint primary key, 
-	inn bigint not null, 
+	inn bigint not null unique, 
 	birthday date not null); 
 create table Practice (
-	id bigint primary key, 
-	pra_title text not null); 
+	pra_id bigint primary key, 
+	pra_title text not null unique); 
 create table Project (
-	pro_title text primary key, 
+	pro_id bigint not null,
+	pro_title text not null unique, 
 	datestart date not null, 
 	dateend date not null, 
 	price bigint not null, 
-	id_practice bigint references Practice(id)); 
+	id_practice bigint not null,
+	primary key (pro_id),
+	foreign key (id_practice) references Practice(pra_id)); 
 create table Department (
-	dep_title text primary key, 
-	id_practice bigint references 
-	Practice(id)); 
+	dep_id bigint not null,
+	dep_title text not null unique, 
+	id_practice bigint not null,
+	primary key (dep_id),
+	foreign key (id_practice) references Practice(pra_id)); 
 create table Job (
 	passport bigint references Employee(passport), 
-	dep_title text references 
-	Department(dep_title), 
+	dep_id bigint references Department(dep_id), 
 	post text not null, 
-	primary key(passport, dep_title)); 
+	primary key(passport, dep_id, post)); 
 create table GetIn (
 	passport bigint references Employee(passport), 
-	pro_title text references Project(pro_title), 
-	primary key(passport,pro_title));
+	pro_id bigint references Project(pro_id),
+	primary key(passport,pro_id));
